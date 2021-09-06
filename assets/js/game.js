e1364aaa78c6169294e9c2920f0b93c9667a6e8c
Paige Huffman <paigehuffman@Paiges-Air-3.cable.rcn.com>
2021-09-05 20:28:26 -0400

    random attack and health values

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,7 +19,6 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract from playerMoney for skipping
-        debugger;
         playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
@@ -31,7 +30,8 @@ var fight = function(enemyName) {
     }
    
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = Math.max(0, enemyHealth - playerAttack);
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
@@ -45,7 +45,8 @@ var fight = function(enemyName) {
     }
 
     // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = Math.max(0, playerHealth - enemyAttack);
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
